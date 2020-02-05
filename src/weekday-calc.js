b6ef2a3fee5b067d6c3575e701cc974ebc2ac9d1
mai cook and rachel schieferstein <mai@>
2020-02-05 13:09:57 -0800

    fixed spacing

--- src/weekday-calc.js
+++ src/weekday-calc.js
@@ -1,18 +1,18 @@
 export class Date {
   constructor(day, month, year) {
-  this.day = day;
-  this.month = month;
-  this.year = year;
+    this.day = day;
+    this.month = month;
+    this.year = year;
   }
 
-minusYear() {
+  minusYear() {
     if (this.month < 3) {
       return this.year -= 1;
+    }
   }
-}
- calculate() {
-  const t = [0,3,2,5,0,3,5,1,4,6,2,4];
-   let weekdaynum = (this.year + Math.round(this.year / 4) - Math.round(this.year / 100) + Math.round(this.year / 400) + t[this.month - 1] + this.day) % 7; 
+  calculate() {
+    const t = [0,3,2,5,0,3,5,1,4,6,2,4];
+    let weekdaynum = (this.year + Math.round(this.year / 4) - Math.round(this.year / 100) + Math.round(this.year / 400) + t[this.month - 1] + this.day) % 7; 
     return weekdaynum;
- }
+  }
 }
